dde5a51b04fc63553b4338f6d9ed43a26bcf7d9d
Jean-luc001 <123200807+Jean-luc001@users.noreply.github.com>
2023-01-22 17:26:30 +0000

    Update ajouter.php

--- ajouter.php
+++ ajouter.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ajouter</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <?php
+
+  <?php
        //vérifier que le bouton ajouter a bien été cliqué
        if(isset($_POST['button'])){
            //extraction des informations envoyé dans des variables par la methode POST
@@ -18,9 +9,9 @@
                 //connexion à la base de donnée
                 include_once "connexion.php";
                 //requête d'ajout
-                $req = mysqli_query($con , "INSERT INTO Participant VALUES(NULL, '$nom', '$prenom', '$Numero','$Adresse')");
+                $req = mysqli_query($con , "INSERT INTO participant VALUES(NULL, '$nom', '$prenom', '$Numero','$Adresse')");
                 if($req){//si la requête a été effectuée avec succès , on fait une redirection
-                    header("location: index.php");
+                  
                 }else {//si non
                     $message = "Participant non ajouté";
                 }
@@ -32,8 +23,19 @@
        }
     
     ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajouter</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  
     <div class="form">
-        <a href="index.php" class="back_btn"><img src="images/back.png"> Retour</a>
+        <a href="index.php" class="back_btn"><img src="images/back.png">Voir la liste</a>
         <h2>Ajouter un participant</h2>
         <p class="erreur_message">
             <?php 
@@ -55,6 +57,18 @@
             <input type="text" name="Adresse">
             <input type="submit" value="Ajouter" name="button">
         </form>
+        <p class="erreur_message">
+            <?php 
+            // si la variable message existe , affichons son contenu
+            if(isset($req)){
+                 if(($req)){
+                echo "Ajouter avec succes";
+            }
+              
+        }
+            ?>
+
+        </p>
     </div>
 </body>
 </html>
